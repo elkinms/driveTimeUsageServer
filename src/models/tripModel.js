4f4699be0fde849727c1edde845004ccfb9beac6
Mikhail <elkinms85@gmail.com>
2025-08-16 10:06:59 +0300

    Add userModel, tripModel

--- src/models/tripModel.js
+++ src/models/tripModel.js
@@ -1,1 +1,24 @@
-//TODO
+import mongoose from "mongoose";
+
+const tripSchema = new mongoose.Schema(
+    {
+        userId: { type: String, required: true, lowercase: true, trim: true }, // email of user
+        startTime: { type: Date, default: Date.now },
+        stopTime: { type: Date, default: null },
+        driveTimeSec: { type: Number, default: 0 },
+        touchCount: { type: Number, default: 0 },          // incremented on touch
+        status: { type: String, enum: ["active", "completed"], default: "active" },
+    },
+    { timestamps: true }
+);
+
+tripSchema.set("toJSON", {
+    transform: (_, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+});
+
+export default mongoose.model("Trip", tripSchema, "trips");
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,22 @@
-//TODO
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: { type: String, lowercase: true, trim: true }, // email as id
+        name: { type: String, trim: true },
+        passwordHash: { type: String, required: true, select: false },
+    },
+    { timestamps: true }
+);
+
+userSchema.set("toJSON", {
+    transform: (_, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.passwordHash;
+        return ret;
+    }
+});
+
+export default mongoose.model("User", userSchema, "users");
