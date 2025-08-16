697391be448f17fbda876d98733717802e8a2758
Mikhail <elkinms85@gmail.com>
2025-08-16 12:43:45 +0300

    Add user routes, trip routes

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import {deleteTrip, getTripById, listTrips, startTrip, stopTrip, touchTrip} from "../controllers/tripControllers.js";
 
 const router = express.Router();
 
-// TODO add routes
+router.post('/start', startTrip);
+router.patch('/:id/touch', touchTrip);
+router.patch('/:id/stop', stopTrip);
+router.get('/', listTrips);
+router.get('/:id', getTripById);
+router.delete('/:id', deleteTrip);
 
 export default router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import {registerUser} from "../controllers/userControllers.js";
+import {
+    changePassword,
+    deleteUser,
+    getUser,
+    loginUser,
+    registerUser,
+    updateUser
+} from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-// TODO add routes
+router.post('/login', loginUser);
+router.get('/:email', getUser);
+router.patch('/:email', updateUser);
+router.patch('/:email/password', changePassword);
+router.delete('/:email', deleteUser);
 
 export default router;
